d00adbe781219d65cc2db1ab2ef24fb1a2310e23
danielsiryakov <danielsiryakov@gmail.com>
2023-06-12 20:11:54 -0400

    whisper deploy

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -8,4 +11,6 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'About'
+    audio_file = open(".\sample1.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
